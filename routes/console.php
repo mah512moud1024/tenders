fce1c687164e8ae026cfda875d7f7ee252fc7eb2
mah512moud1024 <mah512moud1024@gmail.com>
2025-10-25 02:48:21 +0300

    filament admin panel

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Console\Scheduling\Schedule;
 
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('licenses:check-expiry')->daily();
+app(Schedule::class)->command('licenses:check-expiry')->daily();
+
